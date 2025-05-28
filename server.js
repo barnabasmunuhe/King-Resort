1e10f821ead9027b35d4efd3d7589d4b949049b2
Barnabas Milton <162199245+barnabasmunuhe@users.noreply.github.com>
2025-05-29 02:06:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 // Routes
@@ -36,7 +36,7 @@ app.get('/admin', (req, res) => {
 
 // Root redirect to frontend (optional, static index.html page)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  // res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start the server
